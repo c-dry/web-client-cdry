697fc4afc4b0d38ac41b505292ece99f7d62e0ee
diasantoso <mikhaelmds@gmail.com>
2017-04-27 17:07:26 +0700

    benerin lagi dan lagi

--- application/views/order_view.php
+++ application/views/order_view.php
@@ -44,25 +44,10 @@
           <!-- mini logo for sidebar mini 50x50 pixels -->
           <span class="logo-mini"><b>A</b>SM</span>
           <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b>C-DRY | Admin</b></span>
+          <span class="logo-lg"><b>C-DRY | ADMIN</b></span>
         </a>
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top" role="navigation">
-          <!-- Sidebar toggle button-->
-          <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-            <span class="sr-only">Toggle navigation</span>
-          </a>
-          <!--<div class="navbar-custom-menu">
-            <ul class="nav navbar-nav">
-              <!-- User Account: style can be found in dropdown.less -->
-              <!--<li class="dropdown">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Selamat Datang, User <span class="caret"></span></a>
-                      <ul class="dropdown-menu" role="menu">
-                        <li><a href="#">Logout</a></li>
-                      </ul>
-                  </li>
-            </ul>
-          </div>!-->
         </nav>
       </header>
       <!-- Left side column. contains the logo and sidebar -->
@@ -71,7 +56,7 @@
         <section class="sidebar">
           <!-- sidebar menu: : style can be found in sidebar.less -->
           <ul class="sidebar-menu">
-            <li class="header">MAIN NAVIGATION</li>
+            <li class="header">MAIN MENU</li>
             <li class="">
               <a href="<?php echo site_url('')?>">
                 <i class="fa fa-user"></i> <span>User Manage</span>
@@ -96,7 +81,7 @@
         <section class="content">
         <!--<h1 style="font-size:20pt">Ajax CRUD with Bootstrap modals and Datatables with Server side Validation</h1>!-->
 
-        <h3>Data Order</h3>
+        <h3><strong>ORDER DATA</strong></h3>
         <br />
         <button class="btn btn-success" onclick="add_order()"><i class="glyphicon glyphicon-plus"></i> Add Order</button>
         <button class="btn btn-default" onclick="reload_table()"><i class="glyphicon glyphicon-refresh"></i> Reload Table Data</button>
