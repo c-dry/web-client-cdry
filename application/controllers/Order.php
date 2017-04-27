1301fcf9b359e6189cc174063de3555b68087c6b
diasantoso <mikhaelmds@gmail.com>
2017-04-27 16:42:32 +0700

    benerin lagi

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -5,7 +5,7 @@ class Order extends CI_Controller {
 
 	public function __construct()
 	{
-		parent::__construct();		
+		parent::__construct();
 		$this->load->model('order_model','order');
 		$this->load->helper(['url','html','form']);
 		$this->load->database();
@@ -29,16 +29,17 @@ class Order extends CI_Controller {
 			$no++;
 			$row = array();
 			$row[] = $order->email;
+			$row[] = $order->address;
 			$row[] = $order->weight;
 			$row[] = $order->price;
 			$row[] = $order->date_order;
 			$row[] = $order->date_end;
 			$row[] = $order->status;
-		
-			$row[] = 
+
+			$row[] =
 			'<a class="btn btn-sm btn-primary" href="javascript:void(0)" title="Edit" onclick="edit_order('."'".$order->id_order."'".')"><i class="glyphicon glyphicon-pencil"></i> Ubah</a>
 			<a class="btn btn-sm btn-danger" href="javascript:void(0)" title="Hapus" onclick="delete_order('."'".$order->id_order."'".')"><i class="glyphicon glyphicon-trash"></i> Hapus</a>';
-		
+
 			$data[] = $row;
 		}
 
@@ -52,7 +53,7 @@ class Order extends CI_Controller {
 		echo json_encode($output);
 	}
 
-	
+
 
 	public function ajax_edit($id)
 	{
@@ -63,11 +64,12 @@ class Order extends CI_Controller {
 	}
 
 	public function ajax_add()
-	{		
+	{
 		$this->_validate();
 
 		$data = array(
 				'email' => $this->input->post('email'),
+				'address' => $this->input->post('address'),
 				'weight' => $this->input->post('weight'),
 				'price' => $this->input->post('price'),
 				'date_order' => $this->input->post('date_order'),
@@ -83,14 +85,15 @@ class Order extends CI_Controller {
 	public function edit_data($no)
 	{
 		$data['data'] = $this->order->edit($no);
-		$this->load->view('edit',$data);    	
+		$this->load->view('edit',$data);
 	}
 
 	public function ajax_update()
 	{
 		$this->_validate();
-		$data = array(				
+		$data = array(
 				'email' => $this->input->post('email'),
+				'address' => $this->input->post('address'),
 				'weight' => $this->input->post('weight'),
 				'price' => $this->input->post('price'),
 				'date_order' => $this->input->post('date_order'),
@@ -102,7 +105,7 @@ class Order extends CI_Controller {
 	}
 
 	public function ajax_delete($id,$namefile)
-	{		
+	{
 		$this->order->delete_by_id($id);
 		unlink("./files/".$namefile);
 		echo json_encode(array("status" => TRUE));
@@ -136,7 +139,7 @@ class Order extends CI_Controller {
 			$data['error_string'][] = 'Please select status';
 			$data['status'] = FALSE;
 		}
-		
+
 		if($data['status'] === FALSE)
 		{
 			echo json_encode($data);
